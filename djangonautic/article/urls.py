dcfe93392905eb6279b3bf15fe5b1e8e0eca6c8f
tarun <tarun.bapat710@gmail.com>
2018-06-01 13:31:25 +0530

    new changes added

--- djangonautic/article/urls.py
+++ djangonautic/article/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,re_path
 from . import views
 
+app_name='articles'
+
 urlpatterns = [  
-    path('about/',views.about),
-    re_path(r'^(?P<slug>[\w-]+)/$',views.article_details)
+    path('about/',views.about,name="list"),
+    re_path(r'^(?P<slug>[\w-]+)/$',views.article_details,name="detail")
    
 ]
